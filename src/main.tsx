0a98c072614f1c08cee389e4501c28ae909a85c0
Lino4ka00 <adiletkonarbaev7@gmail.com>
2025-02-26 18:53:56 +0300

    ok auth + profile

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'src/reset.css'
+import 'src/index.css'
+
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+
+
+  </StrictMode>,
+)
